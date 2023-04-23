29565059060490bbe2f932d1c4d88f124683b5bf
AndreiZiatiev <83706588+AndreiZiatiev@users.noreply.github.com>
2023-04-23 21:31:24 +0300

    login session page
    login session page

--- front-end/studio/src/app/pages/settings/loginsessions/loginsessions.component.ts
+++ front-end/studio/src/app/pages/settings/loginsessions/loginsessions.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import {AbstractPageComponent} from "../../../components/page-base.component";
+import { LoginSessionService } from '../../../services/login-session.service';
+import {ActivatedRoute} from "@angular/router";
+import {Title} from "@angular/platform-browser";
+import { LoginSession } from '../../../models/login-session';
 @Component({
   selector: 'editor-loginsessions',
   templateUrl: './loginsessions.component.html',
   styleUrls: ['./loginsessions.component.css']
 })
-export class LoginsessionsComponent implements OnInit {
+export class LoginsessionsComponent extends AbstractPageComponent {
 
-  constructor() { }
+  private sessions: LoginSession[];
 
-  ngOnInit(): void {
-  }
+  constructor(private loginSession: LoginSessionService, route: ActivatedRoute, titleService: Title) {
+    super(route, titleService);
+}
+
+protected pageTitle(): string {
+  return "DJAI - Settings - Login Sessions";
+}
+
+public loadAsyncPageData(): void {
+  this.loginSession.getLoginSession().then( sessions => {
+      this.sessions = sessions;
+      this.loaded("sessions");
+  }).catch( error => {
+      console.error("[LoginSessionsPageComponent] Error fetching login sessions.");
+      this.error(error);
+  });
+}
+
+ 
 
 }
